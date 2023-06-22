e8d5f821d7aaa1b9f621f08fb9abbc275ac13f7d
jordansaker <jordan.saker@ymail.com>
2023-06-22 21:15:22 +1000

    updated auth docstring

--- src/blueprints/auth_bp.py
+++ src/blueprints/auth_bp.py
@@ -1,4 +1,4 @@
-'''Authentication Blueprint
+"""Authentication Blueprint
 
 Contains routes related to user authentication.
 
@@ -7,12 +7,17 @@ Routes:
     /login  - 
 
     /register - 
-'''
+"""
 from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['POST'])
 def user_login():
-    '''User login view function. Takes the post body and authenticates the user'''
+    """User login view function. 
+
+    Takes the post body and authenticates the user
+    """
     return {'msg': 'hello'}, 200
+
+
